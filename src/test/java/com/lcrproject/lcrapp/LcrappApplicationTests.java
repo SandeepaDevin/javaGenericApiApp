ed76641350f2f2ae712d50468fd60a4f4a64b8f9
SandeepaDevin <Sandeepdevin@gmail.com>
2025-08-17 23:19:20 +0530

    git testst

---
+++ src/test/java/com/lcrproject/lcrapp/LcrappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcrproject.lcrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcrappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
